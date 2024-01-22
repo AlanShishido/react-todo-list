3726cb9a9ca8c97ce9360978db72dc0ce31e4e1a
Hideyuki Shishido <shishido.hideyuki@gmail.com>
2024-01-21 20:21:27 -0400

    Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import './global.css'
+
+import { Header } from './components/Header'
+
 export function App() {
   return (
-    <h1>Hello Worlds</h1>
+    <>
+      <Header />
+
+      <div>
+        <form >
+          <textarea name="todo" placeholder='Adicione uma nova tarefa'/>
+          <button>
+            Criar
+            <img src="" alt="+" />
+          </button>
+        </form>
+      </div>
+
+      <div>
+        <section>
+          <span>Tarefas criadas 4</span>
+          <span>Conluídas 2 de 4</span>
+        </section>
+
+        <li>
+
+        </li>
+
+        
+      </div>
+    </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css'
+import todoLogo from '../../public/todo-logo.svg'
+
+export function Header () {
+
+    return (
+        <header className={styles.header}>
+            <img src={todoLogo} alt="TODO" />
+        </header>
+    )
+}
